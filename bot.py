3cb4f68af3006033f1d89f31392b706507cd08ab
berkingurcan <berkingurcan@yandex.com>
2024-01-24 14:32:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import os
 import requests
 from dotenv import load_dotenv
+from discord import app_commands
 
 load_dotenv(override=True)
 
@@ -9,6 +10,7 @@ intents = discord.Intents.default()
 intents.messages = True
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 endpoint = "https://zkappsumstad.com/api/evalapi/"
 
@@ -16,8 +18,16 @@ api_key=os.getenv("OPENAI_API_KEY")
 
 @client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=1153348653122076673))
     print(f"We have logged in as {client.user}")
 
+@tree.command(
+    name="umstad",
+    description="Call umstad Command",
+    guild=discord.Object(id=1153348653122076673)
+)
+async def on_command(interaction: discord.Interaction):
+    await interaction.response.send_message("message")
 
 @client.event
 async def on_message(message):
@@ -35,6 +45,8 @@ async def on_message(message):
 
         print(api_response)
         response_content = api_response.content.decode('utf-8')
+
+        # If it is turbo discord etc. remove if :D
         if len(response_content) > 2000:
             response_content = response_content[:2000]
         await message.channel.send(response_content)
