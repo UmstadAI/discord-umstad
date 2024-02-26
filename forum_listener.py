81e3f935489c1ca29f05b8b2f2c7aa6de6dd736f
berkingurcan <berkingurcan@yandex.com>
2024-02-26 13:20:19 +0300

    feat add tag filtering for auto answers

--- forum_listener.py
+++ forum_listener.py
@@ -1,18 +1,22 @@
 import requests
-from config import API_ENDPOINT, API_KEY, FORUM_ID, format_output
+from config import API_ENDPOINT, API_KEY, FORUM_ID, TAG_NAME, format_output
 
 
 async def handle_thread_create(thread):
     if thread.parent_id == FORUM_ID:
-        await thread.fetch_message(thread.id)
-        title = thread.name
-        content = thread.starter_message.content
-        message = title + " " + content
+        includes_qa_tag = any(tag.name == TAG_NAME for tag in thread.applied_tags)
+        if includes_qa_tag:
+            await thread.fetch_message(thread.id)
+            title = thread.name
+            content = thread.starter_message.content
+            message = title + " " + content
 
-        api_response = requests.post(
-            API_ENDPOINT, json={"message": message, "previewToken": API_KEY,},
-        )
+            api_response = requests.post(
+                API_ENDPOINT, json={"message": message, "previewToken": API_KEY,},
+            )
 
-        response_content = api_response.content.decode("utf-8")
+            response_content = api_response.content.decode("utf-8")
 
-        await thread.send(format_output(response_content))
+            await thread.send(format_output(response_content))
+        else:
+            pass
