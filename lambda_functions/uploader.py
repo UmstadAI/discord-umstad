085701f8af5f8d1f30df2350c6d65c422bca0dfe
berkingurcan <berkingurcan@yandex.com>
2024-04-08 22:14:52 +0300

    init uploader

--- lambda_functions/uploader.py
+++ lambda_functions/uploader.py
@@ -1,1 +1,24 @@
-# Uploader to Pinecone
+import glob
+import os
+import openai
+import pinecone
+import time
+import re
+import json
+
+from uuid import uuid4
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv(), override=True)  # read local .env file
+
+pinecone_api_key = os.getenv("PINECONE_API_KEY") or "YOUR_API_KEY"
+pinecone_env = os.getenv("PINECONE_ENVIRONMENT") or "YOUR_ENV"
+
+pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
+
+client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY") or "OPENAI_API_KEY")
+
+index_name = "zkappumstad"
+model_name = "text-embedding-3-small"
+
+VECTOR_TYPE = 'search'
