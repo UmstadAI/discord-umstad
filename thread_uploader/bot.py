13ccc41092451df07244b161ca3726c08f88ac52
berkingurcan <berkingurcan@yandex.com>
2024-04-18 14:10:12 +0300

    Create bot.py for thread uploader

---
+++ thread_uploader/bot.py
@@ -0,0 +1,5 @@
+# Discord Bot has access a forum channel
+# Decide forum thread is answered or solved or closed(How to decide)
+# Gets the thread data
+# Process the Data (with gpt or not?)
+# Upsert it to Vector DB
