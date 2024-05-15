60d6bb349df49b7570653d27ac0ea932b0b50977
berkingurcan <berkingurcan@yandex.com>
2024-05-15 16:33:38 +0300

    feat add thread processor

--- thread_uploader/process_thread.py
+++ thread_uploader/process_thread.py
@@ -1,6 +1,37 @@
 # TODO!: Process thread with all messages and ids etc.
 # TODO!: Add API to main.py for also processed threads
 
+from config import (
+    FORUM_ID,
+    GUILD_ID,
+)
+
 
 async def process_thread(thread):
     print("Processing: ", thread)
+
+    # Return it
+    payload_thread = {}
+
+    guild_id = int(GUILD_ID)
+    thread_id = int(thread.id),
+    title = str(thread.name)
+    messages = []
+    created_at = str(thread.created_at)
+    owner_id = str(thread.owner_id)
+
+
+    async for message in thread.history(limit=100):
+        message_id = message.id
+        content = message.content
+        author = message.author.name
+
+        message = {
+            "Message ID": message_id,
+            "Author": author,
+            "Message Content": content
+        }
+
+        messages.append(message)
+    
+    print(messages)
