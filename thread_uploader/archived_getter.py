ff9d10b512844b1b3c5d03472b6585a945b05df5
berkingurcan <berkingurcan@yandex.com>
2024-06-24 11:16:01 +0300

    feat optimize async the code and add logging

--- thread_uploader/archived_getter.py
+++ thread_uploader/archived_getter.py
@@ -1,17 +1,14 @@
-#  Get threads
-# Process with process_thread
-# Upsert them with batches.
-
 import discord
 import asyncio
 import os
 import sys
 import json
+import aiofiles
+import logging
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
-
 
 from process_thread import process_thread
 from config import (
@@ -21,7 +18,6 @@ from config import (
 )
 
 from dotenv import load_dotenv
-
 load_dotenv(override=True)
 
 from typing import AsyncIterator
@@ -33,30 +29,39 @@ intents.guilds = True
 
 client = discord.Client(intents=intents)
 
+logging.basicConfig(level=logging.INFO)
+
+async def fetch_and_process_threads(channel):
+    async for thread in channel.archived_threads(limit=None):
+        yield thread
 
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    logging.info(f"Logged in as {client.user}")
 
     guild = discord.utils.get(client.guilds, id=GUILD_ID)
     channel = discord.utils.get(client.get_all_channels(), id=FORUM_CHANNEL_ID)
 
     if guild is None or channel is None:
-        print("Guild or Channel not found")
+        logging.error("Guild or Channel not found")
         return
 
-    channel_threads: AsyncIterator = channel.archived_threads(limit=None)
-    archived_threads: list[discord.Thread] = [t async for t in channel_threads]
-
+    semaphore = asyncio.Semaphore(10)  # Limit concurrent tasks
     payloads = []
 
-    for counter, thread in enumerate(archived_threads):
-        if thread:
-            payload = await process_thread(thread)
-            payloads.append(payload)
+    async def process_and_collect(thread):
+        async with semaphore:
+            if thread:
+                try:
+                    payload = await process_thread(thread)
+                    payloads.append(payload)
+                except Exception as e:
+                    logging.error(f"Error processing thread {thread.id}: {e}")
 
-    with open("payloads.json", "w") as f:
-        json.dump(payloads, f, indent=4)
+    tasks = [process_and_collect(thread) async for thread in fetch_and_process_threads(channel)]
+    await asyncio.gather(*tasks)
 
+    async with aiofiles.open("payloads.json", "w") as f:
+        await f.write(json.dumps(payloads, indent=4))
 
 client.run(DISCORD_TOKEN)
